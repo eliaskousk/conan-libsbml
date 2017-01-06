beb14a33b4d971f00e7938e597328ad28dcc9442
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-01-06 11:17:42 +0200

    Do not build x86 packages on OSx

--- build.py
+++ build.py
@@ -1,8 +1,19 @@
 from conan.packager import ConanMultiPackager
+import os
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="eliaskousk")
+
     builder.add_common_builds(shared_option_name="libsbml:shared", pure_c=False)
+
+    if platform.system() == "Darwin":
+        filtered_builds = []
+        for settings, options in builder.builds:
+            # Some isues with OSx and x86 with libxml2 (link with iconv),
+            if settings["arch"] != "x86":
+                filtered_builds.append([settings, options])
+        builder.builds = filtered_builds
+
     builder.run()
 
